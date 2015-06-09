5d7c4585999cb646c622b470ec5538b8a5e07678
chjunginger <chjunginger@users.noreply.github.com>
2015-06-09 15:58:32 +0200

    Update ngrams.py

--- ngrams.py
+++ ngrams.py
@@ -1,48 +1,45 @@
 __author__ = 'christianjunginger'
-# calculates 3-grams from all the lyrics; stores them in textfiles, seperate for each year as a key-value pair.
-#key=3gram; value=number of songs that used the 3-gram that year
+#gets  all ngrams and counts how many distinct songs use it (per year)
 
 import re
 import json
+n=3 #length of ngram
 
 
-
-
-def countwords(lyric):
-    lyricdic=[]
+def PreprocessLyric(lyric):
     lyric = re.sub(r'[^\w\s\']',' ',lyric)
     lyric=lyric.lower()
-    text=lyric.split()
-
-    for i in range(len(text)-2):
-        word="%s %s %s"%(text[i],text[i+1],text[i+2])
-        if word not in lyricdic:
-            if word in wordcount:
-                wordcount[word]+=1
-            else:
-                wordcount[word]=1
-        lyricdic.append(word)
+    return lyric.split()
 
 
+def GetNgramSongCountsForYear(AllLyricsOfYear):
+    for OneTo100 in AllLyricsOfYear:
+        AlreadyCountedNgramOfLyric=[]
+        text=PreprocessLyric(AllLyricsOfYear[OneTo100])
 
-for yr in range(1956,2015):
-    print(yr)
-    #open and read the json file
+        for i in range(len(text)-(n-1)):
+            ngram="%s %s %s"%(tuple(text[i:i+n]))
+            if ngram not in AlreadyCountedNgramOfLyric:
+                AlreadyCountedNgramOfLyric.append(ngram)
+                if ngram in wordcount:
+                    wordcount[ngram]+=1
+                else:
+                    wordcount[ngram]=1
+    return wordcount
+
+
+
+
+for Year in range(1956,1957):
+    print(Year)
     wordcount={}
-    lyricsdic={}
-    g=open('prep_lyrics_%s.txt'%yr)
-    data=json.loads(g.read())
-    fo=open("3_grms_%s.txt"%yr,"wb")
-
-    for y in data:
-        countwords(data[y])
-        i=1
+    AllLyricsOfYear=json.loads(open('prep_lyrics_%s.txt'%Year).read())
+    fo=open("%i_grms_%s.txt"%(n,Year),"wb")
+    NgramCountList=GetNgramSongCountsForYear(AllLyricsOfYear)
 
 
-    for w in sorted(wordcount,key=wordcount.get,reverse=True):
-        fo.write ("%i,%s,%s\n"%(i,w,wordcount[w]))
-        i+=1
-    #fo.write("%i,%s,%i\n"%(i,'not found lyrics', notfoundcounter))
+    for (i,w) in enumerate(sorted(NgramCountList,key=NgramCountList.get,reverse=True)):
+        fo.write ("%i,%s,%s\n"%(i+1,w,NgramCountList[w]))
     fo.close()
 
 
