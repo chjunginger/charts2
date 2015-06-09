7109fda971b3afdcc0e28b6531a3c84e1062aa54
chjunginger <chjunginger@users.noreply.github.com>
2015-06-09 15:29:13 +0200

    Update pulling_lyrics.py

--- pulling_lyrics.py
+++ pulling_lyrics.py
@@ -1,38 +1,31 @@
 __author__ = 'christianjunginger'
 #skript pulls the songs and artists from wikipedias billboard top 100 singles pages and stores it in billboard.txt
-
-
-
 import urllib2
 import re
 import json
 import time
 
 
+def GetTop100forYear(Year):
+    sourcecode=GetSourceCodeofTop100WikiPageForYear(Year)
+
+    if yr>1981:
+        return re.findall('<th scope=\"row\">([0-9]+?)</th>\n<td>\"<.*? title=\".*?\">(.*?)</a>\"</td>\n<td>.*?\">(.*?)</a>',sourcecode)
+    else:
+        return re.findall('<tr>\n<td>([0-9]+?)</td>\n<td(?:>\"<a .*?\stitle=\".*?\")?>(.*?)(?:</a>\")?</td>\n<td>.*?\">(.*?)</a>',sourcecode)
+
+def GetSourceCodeofTop100WikiPageForYear(Year):
+    link=urllib2.urlopen('http://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_%s'%Year)
+    text=link.read()
+    text=re.sub(ur"\xe2\x80\x9c|\xe2\x80\x9d","\"",text)
+    return text
 
 chartthisyear={}
+
 with open('billboard.txt','w') as f:
-
-#Returns a list of list with top 100 (#,song,artist) through the years 1956-2014
-    for yr in range(1956,2015):
-        #to avoid 503-errors
-        time.sleep(2)
-        wordcount={}
-        lyricsdic={}
-        notfoundcounter=0
-
-        #pull the source code from wikipedia's Billboard annual Top 100 pages
-        link=urllib2.urlopen('http://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_%s'%yr)
-        text=link.read()
-        text=re.sub(ur"\xe2\x80\x9c|\xe2\x80\x9d","\"",text)
-
-        #the wikipage changed in starting that year
-        if yr>1981:
-            x=re.findall('<th scope=\"row\">([0-9]+?)</th>\n<td>\"<.*? title=\".*?\">(.*?)</a>\"</td>\n<td>.*?\">(.*?)</a>',text)
-        else:
-            x=re.findall('<tr>\n<td>([0-9]+?)</td>\n<td(?:>\"<a .*?\stitle=\".*?\")?>(.*?)(?:</a>\")?</td>\n<td>.*?\">(.*?)</a>',text)
-
-        chartthisyear[yr]=x
+    for Year in range(1956,2015):
+        time.sleep(2) # avoid 503 errors
+        chartthisyear[Year]=GetTop100forYear(Year)
 
     json.dump(chartthisyear,f,sort_keys=True,indent=4,ensure_ascii=True)
 
